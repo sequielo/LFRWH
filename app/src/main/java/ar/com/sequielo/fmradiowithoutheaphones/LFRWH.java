3f5cd431aaea08da9f9ca280666d6fb7e3d2e8fd
srccrow <7214961+srccrow@users.noreply.github.com>
2022-06-12 15:49:05 +0200

    Update LFRWH.java

--- app/src/main/java/ar/com/sequielo/fmradiowithoutheaphones/LFRWH.java
+++ app/src/main/java/ar/com/sequielo/fmradiowithoutheaphones/LFRWH.java
@@ -13,21 +13,21 @@ public class LFRWH implements IXposedHookLoadPackage {
         if (!lpparam.packageName.equals("com.caf.fmradio"))
             return;
 
-        //XposedBridge.log("[LFRWH] started");
+        // XposedBridge.log("[LFRWH] started");
 
         // https://github.com/LineageOS/android_vendor_qcom_opensource_fm-commonsys/blob/lineage-19.1/fmapp2/src/com/caf/fmradio/FMRadioService.java#L2219
         // https://github.com/LineageOS/android_vendor_qcom_opensource_fm-commonsys
        findAndHookMethod("com.caf.fmradio.FMRadioService", lpparam.classLoader, "isAntennaAvailable", new XC_MethodReplacement() {
             @Override
             protected Object replaceHookedMethod(XC_MethodHook.MethodHookParam methodHookParam) throws Throwable {
-                //XposedBridge.log("[LFRWH] hooked FMRadioService.isAntennaAvailable");
+                // XposedBridge.log("[LFRWH] hooked FMRadioService.isAntennaAvailable");
                 return true;
             }
         });
         findAndHookMethod("com.caf.fmradio.FMRadio", lpparam.classLoader, "isAntennaAvailable", new XC_MethodReplacement() {
             @Override
             protected Object replaceHookedMethod(XC_MethodHook.MethodHookParam methodHookParam) throws Throwable {
-                //XposedBridge.log("[LFRWH] hooked FMRadio.isAntennaAvailable");
+                // XposedBridge.log("[LFRWH] hooked FMRadio.isAntennaAvailable");
                 return true;
             }
         });
